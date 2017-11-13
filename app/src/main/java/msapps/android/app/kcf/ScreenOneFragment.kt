cbe47165d94a0af9daec0ac181b69458fa6d6df7
mayur <mayursalvi93@gmail.com>
2017-11-13 22:07:49 +0530

    Added spinner in layout Integrated Firebase in app changed color of arrow drawable

--- app/src/main/java/msapps/android/app/kcf/ScreenOneFragment.kt
+++ app/src/main/java/msapps/android/app/kcf/ScreenOneFragment.kt
@@ -7,7 +7,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_screen_one.*
+import kotlinx.android.synthetic.main.fragment_screen_one.view.*
+import msapps.android.app.kcf.firebaseMappingClasses.User
 
 
 /**
@@ -22,14 +26,25 @@ class ScreenOneFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         view1 = inflater!!.inflate(R.layout.fragment_screen_one, container, false)
+
+
         this.activity.fab.setOnClickListener{
-            Log.e("ScreenOneFragment","fab clicked")
+            /*var map = if(inputIsCorrect()){
+                //User has entered all details.. create map to save to database
+                FirebaseDatabase.getInstance().reference.child("USER")
+                        .child(etxt_id.text.trim().toString())
+                        .setValue(User(etxt_name.text.trim().toString(), etxt_mob_no.text.trim()))
+            }*/
         }
         return view1
     }
 
     fun newInstance(parent: ViewGroup) : ScreenOneFragment{
         return ScreenOneFragment()
+    }
+
+    fun inputIsCorrect() : Boolean {
+        return etxt_name.text.trim().length != 0 && etxt_id.text.trim().length !=  0 && etxt_mob_no.text.trim().length == 10 && spinner_gender.selectedItem != null
     }
 
 }
