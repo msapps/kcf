07496cc8da8be0a259e4e38a71a6dfd8c4896b6a
mayur <mayursalvi93@gmail.com>
2017-11-14 21:25:13 +0530

    Modified user class added arrayadapter for spinner saving data in firebase after validation

--- app/src/main/java/msapps/android/app/kcf/ScreenOneFragment.kt
+++ app/src/main/java/msapps/android/app/kcf/ScreenOneFragment.kt
@@ -2,15 +2,16 @@ package msapps.android.app.kcf
 
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_screen_one.*
-import kotlinx.android.synthetic.main.fragment_screen_one.view.*
 import msapps.android.app.kcf.firebaseMappingClasses.User
 
 
@@ -19,24 +20,44 @@ import msapps.android.app.kcf.firebaseMappingClasses.User
  */
 class ScreenOneFragment : Fragment() {
     lateinit var view1 : View
-
+    var selectedGender : String = ""
+    lateinit var genderOptions : Array<String>
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         view1 = inflater!!.inflate(R.layout.fragment_screen_one, container, false)
-
+        genderOptions = arrayOf("Male","Female","Other")
 
         this.activity.fab.setOnClickListener{
-            /*var map = if(inputIsCorrect()){
-                //User has entered all details.. create map to save to database
+            if(inputIsCorrect()){
+                var user = User()
+                user.name = etxt_name.text.trim().toString()
+                user.mobNo = etxt_mob_no.text.trim().toString().toLong()
+                user.gender = selectedGender
                 FirebaseDatabase.getInstance().reference.child("USER")
                         .child(etxt_id.text.trim().toString())
-                        .setValue(User(etxt_name.text.trim().toString(), etxt_mob_no.text.trim()))
-            }*/
+                        .setValue(user)
+                Snackbar.make(view1, "Data saved successfully", Snackbar.LENGTH_SHORT).show()
+            }else
+                Snackbar.make(view1, "Please fill all the details",Snackbar.LENGTH_SHORT).show()
         }
         return view1
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        spinner_gender.adapter = ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, genderOptions)
+        spinner_gender.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                selectedGender = genderOptions[position]
+            }
+        }
     }
 
     fun newInstance(parent: ViewGroup) : ScreenOneFragment{
@@ -44,7 +65,7 @@ class ScreenOneFragment : Fragment() {
     }
 
     fun inputIsCorrect() : Boolean {
-        return etxt_name.text.trim().length != 0 && etxt_id.text.trim().length !=  0 && etxt_mob_no.text.trim().length == 10 && spinner_gender.selectedItem != null
+        return etxt_name.text.isNotEmpty() && etxt_id.text.isNotEmpty() && etxt_mob_no.text.trim().length == 10 && selectedGender.isNotEmpty()
     }
 
 }
--- app/src/main/java/msapps/android/app/kcf/firebaseMappingClasses/User.kt
+++ app/src/main/java/msapps/android/app/kcf/firebaseMappingClasses/User.kt
@@ -3,4 +3,8 @@ package msapps.android.app.kcf.firebaseMappingClasses
 /**
  * Created by mayur on 13/11/17.
  */
-data class User(var name : String = "", var mobNo : Long = 0, var gender : String = "")
+class User{
+    var name : String = ""
+    var mobNo : Long = 0
+    var gender : String = ""
+}
