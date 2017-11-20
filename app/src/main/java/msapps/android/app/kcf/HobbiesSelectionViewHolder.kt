7cf23e25324b406ff3ef68a2611e9c32770c9031
mayur <mayursalvi93@gmail.com>
2017-11-20 22:19:54 +0530

    added adapter for recyclerview hobbies created layout for showing hobbies added drawables for green tick changes in gradle for databinding

--- app/src/main/java/msapps/android/app/kcf/HobbiesSelectionViewHolder.kt
+++ app/src/main/java/msapps/android/app/kcf/HobbiesSelectionViewHolder.kt
@@ -2,11 +2,15 @@ package msapps.android.app.kcf
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.synthetic.main.layout_hobby_selection_adapter.view.*
+import msapps.android.app.kcf.firebaseMappingClasses.Hobbies
 
 /**
  * Created by mayur on 19/11/17.
  */
 class HobbiesSelectionViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-
+    fun bind(model: Hobbies?){
+        itemView.txtHobbieName.text = model?.HN ?: "Oops!"
+    }
 
 }
--- app/src/main/java/msapps/android/app/kcf/ScreenTwoFragment.kt
+++ app/src/main/java/msapps/android/app/kcf/ScreenTwoFragment.kt
@@ -4,12 +4,13 @@ package msapps.android.app.kcf
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.fragment_screen_two.*
 import msapps.android.app.kcf.firebaseMappingClasses.Hobbies
 
 
@@ -17,17 +18,14 @@ import msapps.android.app.kcf.firebaseMappingClasses.Hobbies
  * A simple [Fragment] subclass.
  */
 class ScreenTwoFragment : Fragment() {
-lateinit var view1 : View
+    lateinit var view1 : View
+    lateinit var rv_hobbies : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         view1 = inflater!!.inflate(R.layout.fragment_screen_two, container, false)
-        return view1
-    }
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        rv_hobbies = view1.findViewById(R.id.rv_hobbies)
         var linearLayoutManager = LinearLayoutManager(activity.applicationContext)
         linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
         rv_hobbies.layoutManager = linearLayoutManager
@@ -36,9 +34,19 @@ lateinit var view1 : View
                 .setLifecycleOwner(this)
                 .build()
 
-        //var hobbiesAdapter = FirebaseRecyclerAdapter<Hobbies, HobbiesSelectionViewHolder>
+        var hobbiesAdapter = object : FirebaseRecyclerAdapter<Hobbies, HobbiesSelectionViewHolder>(options){
+            override fun onBindViewHolder(holder: HobbiesSelectionViewHolder?, position: Int, model: Hobbies?) {
+                holder?.bind(model)
+            }
 
+            override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): HobbiesSelectionViewHolder {
+                return HobbiesSelectionViewHolder(LayoutInflater.from(parent!!.context).inflate(R.layout.layout_hobby_selection_adapter,parent, false))
+            }
 
+        }
+
+        rv_hobbies.adapter = hobbiesAdapter
+        return view1
     }
 
 }// Required empty public constructor
